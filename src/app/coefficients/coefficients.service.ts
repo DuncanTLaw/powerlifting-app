e604a9cec6808b7fa83c27981c9a158319406fa0
Duncan Law <lawdtk@gmail.com>
2021-05-28 12:30:09 +0100

    Add wilks calc

--- src/app/coefficients/coefficients.service.ts
+++ src/app/coefficients/coefficients.service.ts
@@ -4,6 +4,7 @@ import { GenderCoeff } from '../numeric-tables/coefficient.model';
 import { DOTSCOEFF } from '../numeric-tables/dots';
 import { GLCOEFF } from '../numeric-tables/GL';
 import { IPFCOEFF } from '../numeric-tables/IPF';
+import { WILKSCOEFF } from '../numeric-tables/wilks';
 
 @Injectable({
   providedIn: 'root',
@@ -22,16 +23,19 @@ export class CoeffService {
   ipfScale: GenderCoeff = IPFCOEFF;
   ipfPoints: number;
 
+  wilksScale: GenderCoeff = WILKSCOEFF;
+  wilksPoints: number;
+
   calcDOTS(form: NgForm): void {
     this.male = form.value.gender;
     const bw = form.value.weight;
     this.total = form.value.total;
 
-    const c1 = this.male ?  this.dotsScale.male.c1 : this.dotsScale.female.c1;
-    const c2 = this.male ?  this.dotsScale.male.c2 : this.dotsScale.female.c2;
-    const c3 = this.male ?  this.dotsScale.male.c3 : this.dotsScale.female.c3;
-    const c4 = this.male ?  this.dotsScale.male.c4 : this.dotsScale.female.c4;
-    const c5 = this.male ?  this.dotsScale.male.c5 : this.dotsScale.female.c5;
+    const c1 = this.male ? this.dotsScale.male.c1 : this.dotsScale.female.c1;
+    const c2 = this.male ? this.dotsScale.male.c2 : this.dotsScale.female.c2;
+    const c3 = this.male ? this.dotsScale.male.c3 : this.dotsScale.female.c3;
+    const c4 = this.male ? this.dotsScale.male.c4 : this.dotsScale.female.c4;
+    const c5 = this.male ? this.dotsScale.male.c5 : this.dotsScale.female.c5;
 
     this.dotsPoints = (this.total > 0)? this.total * 500 / (-c1*bw**4 + c2*bw**3 - c3*bw**2 + c4*bw - c5) : 0;
   }
@@ -46,13 +50,13 @@ export class CoeffService {
     let c3: number;
 
     if (!this.benchOnly) {
-      c1 = this.male ?  this.glScale.male.c1 : this.glScale.female.c1;
-      c2 = this.male ?  this.glScale.male.c2 : this.glScale.female.c2;
-      c3 = this.male ?  this.glScale.male.c3 : this.glScale.female.c3;
+      c1 = this.male ? this.glScale.male.c1 : this.glScale.female.c1;
+      c2 = this.male ? this.glScale.male.c2 : this.glScale.female.c2;
+      c3 = this.male ? this.glScale.male.c3 : this.glScale.female.c3;
     } else {
-      c1 = this.male ?  this.glScale.male.c1b : this.glScale.female.c1b;
-      c2 = this.male ?  this.glScale.male.c2b : this.glScale.female.c2b;
-      c3 = this.male ?  this.glScale.male.c3b : this.glScale.female.c3b;
+      c1 = this.male ? this.glScale.male.c1b : this.glScale.female.c1b;
+      c2 = this.male ? this.glScale.male.c2b : this.glScale.female.c2b;
+      c3 = this.male ? this.glScale.male.c3b : this.glScale.female.c3b;
     }
 
     this.glPoints = (this.total > 0)? this.total * 100 / (c1 - c2*Math.E**(-c3*bw)) : 0;
@@ -69,21 +73,37 @@ export class CoeffService {
     let c4: number;
 
     if (!this.benchOnly) {
-      c1 = this.male ?  this.ipfScale.male.c1 : this.ipfScale.female.c1;
-      c2 = this.male ?  this.ipfScale.male.c2 : this.ipfScale.female.c2;
-      c3 = this.male ?  this.ipfScale.male.c3 : this.ipfScale.female.c3;
-      c4 = this.male ?  this.ipfScale.male.c4 : this.ipfScale.female.c4;
+      c1 = this.male ? this.ipfScale.male.c1 : this.ipfScale.female.c1;
+      c2 = this.male ? this.ipfScale.male.c2 : this.ipfScale.female.c2;
+      c3 = this.male ? this.ipfScale.male.c3 : this.ipfScale.female.c3;
+      c4 = this.male ? this.ipfScale.male.c4 : this.ipfScale.female.c4;
     } else {
-      c1 = this.male ?  this.ipfScale.male.c1b : this.ipfScale.female.c1b;
-      c2 = this.male ?  this.ipfScale.male.c2b : this.ipfScale.female.c2b;
-      c3 = this.male ?  this.ipfScale.male.c3b : this.ipfScale.female.c3b;
-      c4 = this.male ?  this.ipfScale.male.c4b : this.ipfScale.female.c4b;
+      c1 = this.male ? this.ipfScale.male.c1b : this.ipfScale.female.c1b;
+      c2 = this.male ? this.ipfScale.male.c2b : this.ipfScale.female.c2b;
+      c3 = this.male ? this.ipfScale.male.c3b : this.ipfScale.female.c3b;
+      c4 = this.male ? this.ipfScale.male.c4b : this.ipfScale.female.c4b;
     }
 
-    this.ipfPoints = 500 + 100 * (
+    this.ipfPoints = (this.total > 0)?
+      500 + 100 * (
       (this.total - (c1 * Math.log(form.value.weight) - c2)) /
       (c3 * Math.log(form.value.weight) - c4)
-    );
+      ) : 0;
   }
 
+  calcWilks(form: NgForm): void {
+    this.male = form.value.gender;
+    const bw = form.value.weight;
+    this.total = form.value.total;
+
+    const c1 = this.male ? -this.dotsScale.male.c1 : this.dotsScale.female.c1;
+    const c2 = this.male ? this.dotsScale.male.c2 : -this.dotsScale.female.c2;
+    const c3 = this.male ? -this.dotsScale.male.c3 : this.dotsScale.female.c3;
+    const c4 = this.male ? -this.dotsScale.male.c4 : -this.dotsScale.female.c4;
+    const c5 = this.male ? this.dotsScale.male.c5 : this.dotsScale.female.c5;
+    const c6 = this.male ? -this.dotsScale.male.c5 : -this.dotsScale.female.c5;
+
+    this.wilksPoints = (this.total > 0)?
+      500 / (c1 + c2*bw + c3*bw**2 + c4*bw**3 + c5*bw**4 + c6*bw**5) : 0;
+  }
 }
