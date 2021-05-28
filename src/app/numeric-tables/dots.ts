828a3007faae84fe09cc39680ab72c87a68c8321
Duncan Law <lawdtk@gmail.com>
2021-05-28 10:29:37 +0100

    Define DOTS coeffs

---
+++ src/app/numeric-tables/dots.ts
@@ -0,0 +1,18 @@
+import { GenderCoeffs } from './coefficient.model';
+
+export const DOTSCOEFF: GenderCoeffs = {
+  male: {
+    c1: 1.093*10**(-6),
+    c2: 7.391293*10**(-4),
+    c3: 0.1918759221,
+    c4: 24.0900756,
+    c5: 307.75076
+  },
+  female: {
+    c1: 1.0706*10**(-6),
+    c2: 5.158568*10**(-4),
+    c3: 0.1126655495,
+    c4: 13.6175032,
+    c5: 57.96288
+  },
+};
