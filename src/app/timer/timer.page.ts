cdbd7a71bee01ef92dd2ed28e2da16a719067e90
Duncan Law <lawdtk@gmail.com>
2021-05-30 11:31:09 +0100

    Add working timer

--- src/app/timer/timer.page.ts
+++ src/app/timer/timer.page.ts
@@ -7,45 +7,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimerPage implements OnInit {
   timerRunning = false;
-  timerInstance: any;
-  buttonText = 'Start';
-  minutes: any = '00';
-  seconds: any = '00';
-  counter = 0;
+  userInput = false;
+  timeSelected: number;
+  timeRemaining = 0;
+  timeEnded = false;
+  interval: any;
+  lhsButtonText = 'Cancel';
+  rhsButtonText = 'Start';
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  onToggleTimer() {
-    this.timerRunning = !this.timerRunning;
-    if (this.timerRunning) {
-      this.buttonText = 'Stop';
-      const startTime = Date.now() - this.counter;
-      this.timerInstance = setInterval(() => {
-        this.counter = Date.now() - startTime;
-        this.minutes = Math.floor(this.counter / 60000);
-        this.seconds = Math.floor(Math.floor(this.counter % 60000) / 1000).toFixed(0);
-        if (+this.minutes < 10) {
-          this.minutes = '0' + this.minutes;
-        }
-        if (+this.seconds < 10) {
-          this.seconds = '0' + this.seconds;
-        }
-      });
+  onInputChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    if(+target.value > 0){
+      this.userInput = true;
+      this.timeRemaining = +target.value;
     } else {
-      this.buttonText = 'Resume';
-      clearInterval(this.timerInstance);
+      this.userInput = false;
     }
   }
 
-  onClearTimer() {
+  onClickLHS(): void {
+    clearInterval(this.interval);
     this.timerRunning = false;
-    this.buttonText = 'Start';
-    this.counter = 0;
-    this.seconds = this.minutes = '00';
-    clearInterval(this.timerInstance);
+    this.rhsButtonText = 'Start';
+    this.timeRemaining = this.timeSelected;
+  }
+
+  onClickRHS(): void {
+    this.timerRunning = !this.timerRunning;
+    if(this.timeEnded) {
+      this.timeRemaining = this.timeSelected;
+    }
+    if(this.timerRunning) {
+      this.timeEnded = false;
+      this.rhsButtonText = 'Pause';
+      this.interval = setInterval(() => {
+        if(this.timeRemaining > 0) {
+          this.timeRemaining--;
+        } else {
+          this.rhsButtonText = 'Start';
+          this.timerRunning = false;
+          this.timeEnded = true;
+          clearInterval(this.interval);
+        }
+      }, 1000);
+    } else {
+      this.rhsButtonText = 'Resume';
+      clearInterval(this.interval);
+    }
   }
 
 }
