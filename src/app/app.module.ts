6a05b291abdf792eb54624d65b37c6f11f8367aa
Duncan Law <lawdtk@gmail.com>
2021-08-12 23:55:16 +0100

    Add abouts page accessible from side

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
+import { AboutComponent } from './about/about.component';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, AboutComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
