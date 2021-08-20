f266b07df4a03269bf533a18654dbdd80159d5c4
Duncan Law <lawdtk@gmail.com>
2021-08-20 15:15:59 +0100

    Add iOS files and remove safe areas as Capacitor seems to handle them

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'web.app.dlaw-powerlifting',
+  appName: 'powerlifting-toolboxx',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
