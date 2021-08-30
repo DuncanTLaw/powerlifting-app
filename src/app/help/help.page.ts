46aa3ccbc9fc1157475657b45a82fb99ddeb7a63
Duncan Law <lawdtk@gmail.com>
2021-08-30 22:53:29 +0100

    Route back to last page on close menu

--- src/app/help/help.page.ts
+++ src/app/help/help.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HelpService } from './help.service';
 
 interface Pages {
   title: string;
@@ -53,7 +54,7 @@ export class HelpPage implements OnInit {
     }
   ];
 
-  constructor() { }
+  constructor(public helpService: HelpService) { }
 
   ngOnInit() {
   }
