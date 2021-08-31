54a99cf979ebf91ca5875c930d0a997f2f083ff7
Duncan Law <lawdtk@gmail.com>
2021-08-31 13:11:03 +0100

    Change product id to remove -

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'web.app.dlaw-powerlifting',
+  appId: 'web.app.dlawpowerlifting',
   appName: 'powerlifting-toolbox',
   webDir: 'www',
   bundledWebRuntime: false
