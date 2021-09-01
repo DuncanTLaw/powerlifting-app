0e37ccb0abb244b7ccd451f91b07f342ca8d0e4a
Duncan Law <lawdtk@gmail.com>
2021-09-01 12:53:22 +0100

    Add meta tags for SEO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  title = 'Powerlifting Toolbox';
 
-  constructor() {}
+  constructor(private titleService: Title, private metaService: Meta) {}
+
+  ngOnInit() {
+    this.titleService.setTitle(this.title);
+    this.metaService.addTags([
+      {name: 'keywords', content: 'powerlifting, IPF points, GL points, Wilks, Dots points, bar loader, IPF, USAPL, GBPF'},
+      {name: 'description', content:
+        'Powerlifting toolbox which includes RPE claculator, points calculations, timer, sets counter and what plates to load.'
+      }
+    ]);
+  }
 
 }
