9e9682df8074405afd95e9687ae9739a40d6fc3f
Duncan Law <lawdtk@gmail.com>
2021-09-01 14:01:24 +0100

    Remove unused imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Title, Meta } from '@angular/platform-browser';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,12 +7,10 @@ import { Title, Meta } from '@angular/platform-browser';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent implements OnInit{
-  title = 'Powerlifting Toolbox';
 
-  constructor(private titleService: Title, private metaService: Meta) {}
+  constructor(private metaService: Meta) {}
 
   ngOnInit() {
-    this.titleService.setTitle(this.title);
     this.metaService.addTags([
       {name: 'keywords', content:
         'powerlifting, RPE, rate of perceived exertion, IPF points, GL points, Wilks, Dots points, bar loader, IPF, USAPL, GBPF, gym'
