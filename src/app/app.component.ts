beb944ca105de128dc63de6c7cf5cd014bc3b352
Duncan Law <lawdtk@gmail.com>
2021-09-10 19:43:27 +0100

    Dynamically change ion tab button active colour

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,42 @@ import { Component, OnInit } from '@angular/core';
 import { Meta } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 
+interface PAGES {
+  [pageName: string]: {
+    label: string;
+    icon: string;
+    colour: string;
+  };
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent implements OnInit{
+  pages: PAGES = {
+    rpe: {
+      label: 'RPE',
+      icon: 'stats-chart',
+      colour: 'var(--ion-color-tertiary)'
+    },
+    coeff: {
+      label: 'Coefficients',
+      icon: 'calculator',
+      colour: 'var(--ion-color-warning)'
+    },
+    timer: {
+      label: 'Timer',
+      icon: 'timer-sharp',
+      colour: 'var(--ion-color-danger)'
+    },
+    loader: {
+      label: 'Loader',
+      icon: 'barbell-sharp',
+      colour: 'var(--ion-color-success)'
+    },
+  };
 
   constructor(private metaService: Meta, public router: Router) {}
 
@@ -25,4 +55,7 @@ export class AppComponent implements OnInit{
     ]);
   }
 
+  returnZero(): number {
+    return 0;
+  }
 }
