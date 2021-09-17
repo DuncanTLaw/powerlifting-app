11c056bbdcfa868e27d10586b080352eac815dba
Duncan Law <lawdtk@gmail.com>
2021-09-17 15:59:12 +0100

    Add blues colour

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,6 @@ import { SettingsComponent } from './settings/settings.component';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent implements OnInit{
-  prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
 
   constructor(
     private router: Router,
